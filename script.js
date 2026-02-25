f6438d55bb47c4f27a20560760baada7304ae79b
mahzabin-sayeka <mahzabinsayeka000212@gmail.com>
2026-02-26 02:07:21 +0600

    card count write done

--- script.js
+++ script.js
@@ -44,13 +44,17 @@ function toggleStyle(id){
         allcardsection.classList.remove('hidden');
         filterSection.classList.add('hidden')
     }
+    else if(id == 'rejected-filter-btn'){
+        allcardsection.classList.add('hidden');
+        filterSection.classList.remove('hidden')
+    }
 
 }
 
 mainContainer.addEventListener('click', function(event){
 
-    // console.log(event.target.classList.contains('btn-interview'))
-    
+    // interview part
+
     if(event.target.classList.contains('btn-interview')){
         const parentNode = event.target.closest('.card1');
     const companyName = parentNode.querySelector('.company-name').innerText
@@ -61,6 +65,8 @@ mainContainer.addEventListener('click', function(event){
     const applyBtn = parentNode.querySelector('.apply-button').innerText
     const interviewBtn = parentNode.querySelector('.btn-interview').innerText
     const rejectedBtn = parentNode.querySelector('.btn-rejected').innerText
+    
+    parentNode.querySelector('.apply-button').innerText = 'Interview'
 
     const cardInfo={
         companyName,
@@ -68,25 +74,70 @@ mainContainer.addEventListener('click', function(event){
         location,
         time,
         salary,
-        applyBtn,
+        applyBtn:'Interview',
         interviewBtn,
         rejectedBtn
     }
 
      const companyExist = interviewList.find(item=> item.companyName == cardInfo.companyName)
      
-       parentNode.querySelector('.apply-button').innerText = 'Interested'
+       
 
      if(!companyExist){
         interviewList.push(cardInfo)
      }
+     rejectedList = rejectedList.filter(item=> item.companyName != cardInfo.companyName)
+
+    calculatecount()
+
     renderInterview()
+
+    }
+
+    // rejected part
+
+    else if(event.target.classList.contains('btn-rejected')){
+        const parentNode = event.target.closest('.card1');
+    const companyName = parentNode.querySelector('.company-name').innerText
+    const jobTitle = parentNode.querySelector('.job-title').innerText
+    const location = parentNode.querySelector('.location').innerText
+    const time = parentNode.querySelector('.time').innerText
+    const salary = parentNode.querySelector('.salary').innerText
+    const applyBtn = parentNode.querySelector('.apply-button').innerText
+    const interviewBtn = parentNode.querySelector('.btn-interview').innerText
+    const rejectedBtn = parentNode.querySelector('.btn-rejected').innerText
+    
+    parentNode.querySelector('.apply-button').innerText = 'Rejected'
+
+    const cardInfo={
+        companyName,
+        jobTitle,
+        location,
+        time,
+        salary,
+        applyBtn:'Rejected',
+        interviewBtn,
+        rejectedBtn
+    }
+
+     const companyExist = rejectedList.find(item=> item.companyName == cardInfo.companyName)
+     
+       
+
+     if(!companyExist){
+        rejectedList.push(cardInfo)
+     }
+    interviewList = interviewList.filter(item=> item.companyName != cardInfo.companyName)
+
+    calculatecount()
+
+    renderRejected()
 
     }
 
 })
 
-function renderInterview (){
+function renderInterview(){
     filterSection.innerHTML = '';
 
     for(let interview of interviewList){
@@ -98,22 +149,72 @@ function renderInterview (){
                         <div>
                             <div>
                               <h2 class="company-name text-xl font-bold text-gray-900">${interview.companyName}</h2>
-                              <p class="job-title text-gray-600 font-medium mt-1 pb-4">React Native Developer</p>
+                              <p class="job-title text-gray-600 font-medium mt-1 pb-4">${interview.jobTitle}</p>
                             </div>
                            
                              <div> 
                                 <p>
-                                    <span class="location text-gray-700">Remote</span>
+                                    <span class="location text-gray-700">${interview.location}</span>
                                     <span>•</span>
-                                    <span  class="time text-gray-700">Full-time</span>
+                                    <span  class="time text-gray-700">${interview.time}</span>
                                     <span>•</span>
-                                    <span class="salary text-gray-700">$130,000 - $175,000</span>
+                                    <span class="salary text-gray-700">${interview.salary}</span>
                                 </p>
                            
                             </div>
 
                             <div class="mt-4">
-                                <button class="apply-button px-3 py-1 text-xs font-bold  bg-blue-50 text-black rounded"> NOT APPLIED
+                                <button class="apply-button px-3 py-1 text-xs font-bold  bg-blue-50 text-black rounded"> ${interview.applyBtn}
+                                </button>
+                            </div>
+
+                          <p class="mt-4 text-sm text-gray-700 "> Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+
+                             <div class="mt-6 flex gap-3">
+                               <button class="btn-interview px-5 py-2 text-xs font-bold text-green-700 border-2 border-green-600 rounded">INTERVIEW </button>
+
+                               <button class="btn-rejected px-5 py-2 text-xs font-bold uppercase tracking-widest text-red-700 border-2 border-red-600 rounded "> REJECTED </button>
+                            </div>
+                        </div>
+                            
+                        <div>
+                            <button class="btn-delete"><i class="fa-solid fa-trash-can"></i></button>
+                        </div>
+                    </div>
+                        `
+        filterSection.appendChild(div)
+    }
+}
+
+
+function renderRejected(){
+    filterSection.innerHTML = '';
+
+    for(let reject of rejectedList){
+          
+        let div = document.createElement('div')
+        div.className = 'card1'
+        div.innerHTML = `
+            <div class="card1 bg-white  mt-10 p-6  rounded-lg flex justify-between">
+                        <div>
+                            <div>
+                              <h2 class="company-name text-xl font-bold text-gray-900">${reject.companyName}</h2>
+                              <p class="job-title text-gray-600 font-medium mt-1 pb-4">${reject.jobTitle}</p>
+                            </div>
+                           
+                             <div> 
+                                <p>
+                                    <span class="location text-gray-700">${reject.location}</span>
+                                    <span>•</span>
+                                    <span  class="time text-gray-700">${reject.time}</span>
+                                    <span>•</span>
+                                    <span class="salary text-gray-700">${reject.salary}</span>
+                                </p>
+                           
+                            </div>
+
+                            <div class="mt-4">
+                                <button class="apply-button px-3 py-1 text-xs font-bold  bg-blue-50 text-black rounded"> ${reject.applyBtn}
                                 </button>
                             </div>
 
